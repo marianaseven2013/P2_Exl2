37f8747be4d301a44b6e4dfb212684439482c6d9
marianaseven2013 <gmluch@scl.edu.gt>
2024-07-09 18:19:02 -0600

    Creacion de Header

--- header/header.js
+++ header/header.js
@@ -2,8 +2,13 @@ let holao = document.querySelector(".header");
 
 holao.innerHTML = `
         <div class="elu">
-                <div class="jin">
-                       <h1>hola</h1>
+                <div class="jin">                                 
+                        <a href="">Locales</a>
+                        <a href="">Horarios</a>
+                        <a href="">Precios</a>
+                        <img src="https://i.pinimg.com/564x/7f/f2/61/7ff261a75cd158d20af44791352583a4.jpg" alt="">
+
+                        <div class="Btn">Call Now</div>
                 </div>
         </div>
 `;
