761678fbf83d98894194186a4b28d188d20b7c1d
marianaseven2013 <gmluch@scl.edu.gt>
2024-07-09 22:35:21 -0600

    Creación de sección1(2)

--- seccion2/seccion2.js
+++ seccion2/seccion2.js
@@ -3,7 +3,53 @@ let holas = document.querySelector(".seccionn2");
 holas.innerHTML = `
         <div class="eli">
                 <div class="gigi">
-                       <h1>hola</h1>
+                        <div class="ojo hoja1"><h1>
+                                                Lo más popular en merch!!!
+                                                </h1>
+                        </div>
+                        <div class="ojo hoja2">
+                                <img src="https://i.pinimg.com/564x/8b/b0/ba/8bb0bad38a5f7c16986020c85f39146a.jpg" alt="">
+                        </div>
+                        <div class="ojo hoja3">
+                                <img src="https://i.pinimg.com/564x/1d/2b/8f/1d2b8f06894e2398305d563bce2fcff4.jpg" alt="">
+                        </div>
+                        <div class="ojo hoja4">
+                                <img src="https://i.pinimg.com/564x/4c/e6/3c/4ce63c332aef695ac5aa501954d107a7.jpg" alt="">
+                        </div>
+                        <div class="ojo hoja5">
+                                <img src="https://i.pinimg.com/564x/e0/0a/37/e00a377df00c3578e2be2b76f881078e.jpg" alt="">
+                        </div>
+                        <div class="ojo hoja6">
+                                <img src="https://i.pinimg.com/564x/6c/c7/bb/6cc7bbfd94c460b532f301352c5c5f48.jpg" alt="">
+                        </div>
+                        <div class="ojo moda1">
+                               <a href=""><br>bt21</a> </div>
+                        <div class="ojo moda2">
+                                <a href=""><br>ARMYBOM</a></div>
+                        <div class="ojo moda3">
+                                <a href=""><br>Album Spine Braker</a>
+                        </div>
+                        <div class="ojo moda4">
+                                <a href=""><br>Sudadera Jimin</a>
+                        </div>
+                        <div class="ojo moda5">
+                                <a href=""><br>Photocards</a>
+                        </div>
+                       <!--  <div class="ojo moda6">
+                                <h1>hola</h1>
+                                <img src="c:\Users\genes\Downloads\pngwing.com.png" alt="">
+                        </div>
+                        <div class="ojo moda7">
+                                <img src="c:\Users\genes\Downloads\pngwing.com.png" alt="">
+                        </div>
+                        <div class="ojo moda8">
+                                <img src="c:\Users\genes\Downloads\pngwing.com.png" alt="">
+                        </div>
+                        <div class="ojo moda9">
+                                <img src="c:\Users\genes\Downloads\pngwing.com.png" alt="">
+                        </div> -->
+                       <!--  <div class="ojo moda6">hola</div> -->
+                        
                 </div>
         </div>
 `;
